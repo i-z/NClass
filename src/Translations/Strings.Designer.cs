40ba7d7af1a79f84687e7096d2bfed71f0975a29
Igor Zubkov <igor.z@oneupgames.gg>
2025-01-11 14:54:25 +0300

    wip1

--- src/Translations/Strings.Designer.cs
+++ src/Translations/Strings.Designer.cs
@@ -19,10 +19,10 @@ namespace NClass.Translations {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public partial class Strings {
+    public class Strings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -75,6 +75,15 @@ namespace NClass.Translations {
         public static string Access {
             get {
                 return ResourceManager.GetString("Access", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Add New Abstraction.
+        /// </summary>
+        public static string AddNewAbstraction {
+            get {
+                return ResourceManager.GetString("AddNewAbstraction", resourceCulture);
             }
         }
         
